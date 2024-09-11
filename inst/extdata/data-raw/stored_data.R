53fde0f34b61ea7d6a2564fbd5e217b3f030e0d9
kirstinlyon <kirstin@mltwelve.com>
2024-09-11 13:26:50 +0200

    updated program_area_name mapping

--- inst/extdata/data-raw/stored_data.R
+++ inst/extdata/data-raw/stored_data.R
@@ -1,24 +1,27 @@
 data_program_area_name_map <-
 
-tibble::tribble(
-    ~program_area,                             ~program_area_name,
-           "HL.1",                                     "HIV/AIDS",
-           "HL.2",                                           "TB",
-           "HL.3",                                      "Malaria",
-           "HL.4",                                          "GHS",
-           "HL.5",                  "Health Systems Strenghening",
-           "HL.6",                                          "MCH",
-           "HL.7",                                        "FP/RH",
-           "HL.8",                                         "WASH",
-           "HL.9",                                    "Nutrition",
-           "PO.1",                                         "PD&L",
-           "DR.1",       "Conflict Mitigation and Reconciliation",
-           "DR.2",                     "Democracy and Governance",
-           "DR.3", "Political Competition and Consensus-Building",
-           "DR.4",                                "Civil Society",
-           "DR.5",                           "Peace and Security",
-           "DR.6",                                 "Human Rights",
-           "PO.2",                                          "A&O"
+    tibble::tribble(
+        ~program_area,                             ~program_area_name,
+        "HL.1",                                     "HIV/AIDS",
+        "HL.2",                                           "TB",
+        "HL.3",                                      "Malaria",
+        "HL.4",                                          "GHS",
+        "HL.5",                  "Health Systems Strenghening",
+        "HL.6",                                          "MCH",
+        "HL.7",                                        "FP/RH",
+        "HL.8",                                         "WASH",
+        "HL.9",                                    "Nutrition",
+        "PO.1",                                         "PD&L",
+        "DR.1",       "Conflict Mitigation and Reconciliation",
+        "DR.2",                     "Democracy and Governance",
+        "DR.3", "Political Competition and Consensus-Building",
+        "DR.4",                                "Civil Society",
+        "DR.5",                           "Peace and Security",
+        "DR.6",                                 "Human Rights",
+        "PO.2",                                          "A&O",
+        "ES.1",                              "Basic Education",
+        "EG.3",                                  "Agriculture",
+        "EG.5",                  "Private Sector Productivity",
     )
 usethis::use_data(data_program_area_name_map, overwrite = TRUE)
 
