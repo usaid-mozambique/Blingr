76b70054a8c4c88a6f9debaec851d9a193e36921
kirstinlyon <kirstin@mltwelve.com>
2024-09-16 19:47:55 +0200

    additional cleaning steps added

--- R/clean_phoenix_open_commitments.R
+++ R/clean_phoenix_open_commitments.R
@@ -30,8 +30,16 @@ clean_phoenix_open_commitments <- function(file){
         ) |>
         dplyr::filter(operating_unit == "MOZAMBIQUE") |>
         tidyr::drop_na(document_number) |>
+
+        #rename old program_areas to match the new
+        dplyr::left_join(blingr::data_program_element_map, by = c("program_element" = "old_program_element")) |>
+        dplyr::mutate(program_area = dplyr::if_else(is.na(new_program_area), program_area, new_program_area)) |>
+        dplyr::select(-new_program_area) |>
+
+        #add program area names
+        dplyr::left_join(blingr::data_program_area_name_map, by = "program_area") |>
         dplyr::select(activity, commitment_type, document_number, actg_line,
-                      open_commitment_amt, bfy_fund, operating_unit, program_area,
+                      open_commitment_amt, bfy_fund, operating_unit, program_area, program_area_name,
                       distribution, program_element, original_date,
                       outstanding_days_of_cmmt, cmmt_less_than_30_days,
                       cmmt_amt_between_31_to_90_days, cmmt_amt_between_91_to_180_days,
