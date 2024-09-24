cc7ff6d32b4d1564edc57747690ec15845caaf78
kirstinlyon <kirstin@mltwelve.com>
2024-09-24 14:19:37 +0200

    Added function for creating cumulative column to phoenix transaction dataset

---
+++ R/create_phoenix_transaction_cumulative.R
@@ -0,0 +1,33 @@
+
+#' Create cumulative totals to the complete transaction dataset
+#'
+#' @param phoenix_transaction_df cleaned phoenix transaction dataset
+#'
+#' @return a complete transaction dataset with cumulative totals
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  df <- create_phoenix_transaction_cumulative(transaction_df)
+#'  }
+create_phoenix_transaction_cumulative <- function(phoenix_transaction_df){
+
+    temp <- phoenix_transaction_df |>
+
+    dplyr::group_by(award_number, period, program_area, fiscal_year, quarter) |>
+        dplyr::summarise(dplyr::across(dplyr::where(is.numeric), ~sum(., na.rm = TRUE)), .groups = "drop")
+
+    phoenix_transaction_disbursement_fy <- phoenix_transaction_df |>
+        dplyr::select(award_number, fiscal_year, quarter, program_area, transaction_disbursement, period) |>
+        dplyr::arrange(award_number, fiscal_year, quarter)  |>
+        dplyr::group_by(award_number, fiscal_year, program_area) |>
+        dplyr::mutate(cumulative_transaction_disbursement_fy = cumsum(transaction_disbursement))  |>
+        dplyr::ungroup() |>
+        dplyr::select(-c(fiscal_year, quarter, transaction_disbursement))
+
+    temp <- phoenix_transaction_df |>
+        dplyr::left_join(phoenix_transaction_disbursement_fy, by = c("award_number", "period", "program_area")) |>
+        dplyr::select(-c(fiscal_year, quarter))
+
+    return(temp)
+}
