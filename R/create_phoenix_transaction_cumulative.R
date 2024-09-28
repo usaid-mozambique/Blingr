817e2db56590d6ab0d0d54287da22aab5e8712a9
kirstinlyon <kirstin@mltwelve.com>
2024-09-28 13:00:20 +0200

    bug fix

--- R/create_phoenix_transaction_cumulative.R
+++ R/create_phoenix_transaction_cumulative.R
@@ -2,39 +2,33 @@
 #' Create cumulative totals to the complete transaction dataset
 #'
 #' @param phoenix_transaction_df cleaned phoenix transaction dataset
-#' @param calendar_type calendar type to be used either fiscal or calendar
 #'
 #' @return a complete transaction dataset with cumulative totals
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#'  df <- create_phoenix_transaction_cumulative(transaction_df, fiscal)
+#'  df <- create_phoenix_transaction_cumulative(transaction_df)
 #'  }
-create_phoenix_transaction_cumulative <- function(phoenix_transaction_df, calendar_type = "fiscal"){
-
-    calendar_type <- ifelse(calendar_type == "fiscal", "fiscal_year", "calendar_year")
-
-    # Convert the 'calendar_type' string into a symbol
-    calendar_type_sym <- dplyr::sym(calendar_type)
+create_phoenix_transaction_cumulative <- function(phoenix_transaction_df){
 
     temp <- phoenix_transaction_df |>
 
-    dplyr::group_by(award_number, period, program_area, !!calendar_type_sym) |>
+    dplyr::group_by(award_number, period, program_area, fiscal_year) |>
         dplyr::summarise(dplyr::across(dplyr::where(is.numeric), ~sum(., na.rm = TRUE)), .groups = "drop")
 
     phoenix_transaction_disbursement_fy <- phoenix_transaction_df |>
-        dplyr::select(award_number, !!calendar_type_sym,  program_area, transaction_disbursement, period) |>
-        dplyr::arrange(award_number, !!calendar_type_sym)  |>
-        dplyr::group_by(award_number, !!calendar_type_sym, program_area) |>
+        dplyr::select(award_number, fiscal_year,  program_area, transaction_disbursement, period) |>
+        dplyr::arrange(award_number, fiscal_year)  |>
+        dplyr::group_by(award_number, fiscal_year, program_area) |>
         dplyr::mutate(cumulative_transaction_disbursement_fy = cumsum(transaction_disbursement))  |>
         dplyr::ungroup() |>
-        dplyr::select(-c(!!calendar_type_sym, transaction_disbursement))
+        dplyr::select(-c(fiscal_year, transaction_disbursement))
 
     temp <- phoenix_transaction_df |>
         dplyr::left_join(phoenix_transaction_disbursement_fy, by = c("award_number", "period", "program_area")) |>
-        dplyr::select(-c(!!calendar_type_sym))
+        dplyr::select(-c(fiscal_year))
 
     return(temp)
 }
-?sym
+
