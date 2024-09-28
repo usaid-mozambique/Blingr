49dc93883c41c64cf44fabde4c89e062efb246c8
kirstinlyon <kirstin@mltwelve.com>
2024-09-28 18:30:24 +0200

    Fixed cumulative

--- R/create_phoenix_transaction_cumulative.R
+++ R/create_phoenix_transaction_cumulative.R
@@ -13,24 +13,15 @@
 create_phoenix_transaction_cumulative <- function(phoenix_transaction_df){
 
     # Step 1: Summarize numeric columns grouped by relevant keys
-    temp <- phoenix_transaction_df |>
-        dplyr::group_by(award_number, period, program_area, fiscal_year) |>
-        dplyr::summarise(dplyr::across(dplyr::where(is.numeric), ~sum(., na.rm = TRUE)), .groups = "drop")
-
-    # Step 2: Calculate cumulative transaction disbursement per fiscal year
-    phoenix_transaction_disbursement_fy <- phoenix_transaction_df |>
-        dplyr::select(award_number, fiscal_year, program_area, transaction_disbursement, period) |>
-        dplyr::arrange(award_number, fiscal_year, period)  |>
-        dplyr::group_by(award_number, fiscal_year, program_area) |>
-        dplyr::mutate(cumulative_transaction_disbursement_fy = cumsum(transaction_disbursement)) |>
-        dplyr::ungroup()
+    temp_cumulative <- phoenix_transaction_df |>
+        dplyr::select(fiscal_year, program_area, award_number, transaction_disbursement) |>
+        dplyr::group_by(award_number, program_area, fiscal_year) |>
+        dplyr::summarise(cumulative_transaction_disbursement_fy = sum(transaction_disbursement, na.rm = TRUE), .groups = "drop")
 
     # Step 3: Merge cumulative disbursement data back into the summary
-    temp <- temp |>
-        dplyr::left_join(phoenix_transaction_disbursement_fy,
-                         by = c("award_number", "program_area", "fiscal_year", "period"),
-                         suffix = c("", ".y")) |>
-        dplyr::select(-transaction_disbursement.y)  # Remove this column after merging
+    temp <- phoenix_transaction_df |>
+        dplyr::left_join(temp_cumulative,
+                         by = c("award_number", "program_area", "fiscal_year"))
 
     return(temp)
 }
