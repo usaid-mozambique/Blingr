c41255134b08d42b5e401faa7bad326aef7f662e
kirstinlyon <kirstin@mltwelve.com>
2024-10-14 20:55:47 +0200

    reintroduced cumulative transaction

---
+++ R/create_phoenix_transaction_cumulative.R
@@ -0,0 +1,26 @@
+
+#' Create cumulative totals to the complete transaction dataset
+#'
+#' @param phoenix_transaction_df cleaned phoenix transaction dataset
+#'
+#' @return a complete transaction dataset with cumulative totals
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  df <- create_phoenix_transaction_cumulative(transaction_df)
+#'  }
+create_phoenix_transaction_cumulative <- function(phoenix_transaction_df){
+
+    # Step 1: Summarize numeric columns grouped by relevant keys
+    temp <- phoenix_transaction_df |>
+        dplyr::group_by(award_number, program_area, fiscal_year) |>
+        dplyr::arrange(period) |>
+        dplyr::mutate(
+            transaction_disbursement_cumulative = cumsum(transaction_disbursement)  # Calculate cumulative sum within the year
+        )  |>
+        dplyr::ungroup() |>
+        dplyr::select(-c(fiscal_year, fiscal_quarter, transaction_disbursement))
+
+    return(temp)
+}
