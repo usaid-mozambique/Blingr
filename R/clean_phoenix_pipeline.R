e76b5474041fc5f859952a11c7bd2f0d76379841
kirstinlyon <kirstin@mltwelve.com>
2024-10-14 22:14:56 +0200

    removed unecessary columns

--- R/clean_phoenix_pipeline.R
+++ R/clean_phoenix_pipeline.R
@@ -25,10 +25,9 @@ clean_phoenix_pipeline <- function(PHOENIX_PIPELINE_PATH, all_award_number,
         dplyr::rename_with(~lookup[.x], dplyr::any_of(names(lookup))) |>
         dplyr::filter(obligation_type %in% obligation_type_filter,
                       distribution %in% distribution_filter) |>
-        dplyr::select(document_amt,
-                      obligation_amt, subobligation_amt, disbursement_amt,
-                      undisbursed_amt, last_qtr_accrual_amt,document_number,
-                      program_area, award_number, program_element, pipeline_amt,
+        dplyr::select(document_amt, disbursement_amt,
+                      undisbursed_amt, last_qtr_accrual_amt, document_number,
+                      program_area, award_number, program_element,
                       bilateral_obl_number
         ) |>
 
@@ -38,7 +37,6 @@ clean_phoenix_pipeline <- function(PHOENIX_PIPELINE_PATH, all_award_number,
                       disbursement_amt = as.numeric(disbursement_amt),
                       undisbursed_amt = as.numeric(undisbursed_amt),
                       last_qtr_accrual_amt = as.numeric(last_qtr_accrual_amt),
-                      pipeline_amt = as.numeric(pipeline_amt),
                       award_number = dplyr::case_when(
                           award_number %in% all_award_number ~ award_number,
                           TRUE ~ document_number)
